24d4fd708af6eef61df12ad78c5710e0e857673f
Ivánczi Balázs <balazsivanczi06@gmail.com>
2023-07-30 18:00:04 +0200

    Fixed the screen preset and Menus work now

--- main.py
+++ main.py
@@ -1,55 +1,55 @@
 from click import getchar
-from core import Screen, clr_ln
+from core import Screen, clr_ln, startup
+from pickle import dumps, loads
 
 def main():
     screen = Screen()
-    screen.put('This is where the game goes')
+    startup(screen)
     while True:
         char = getchar()
-        match char:
-            case ':':
-                command = input(':')
-                clr_ln()
-                commands = command.split(' ')
-                match commands[0]:
-                    case 'esc':
-                        continue
-                    case 'wq':
-                        with open('./test.save', 'w', encoding='utf-8') as f:
-                            f.write("saved and quit")
-                        break
-                    case "w":
-                        with open('./test.save', 'w', encoding='utf-8') as f:
-                            f.write("saved")
-                    case 'q!':
-                        print("force quit", end='\r')
-                        break
-                    case 'x':
-                        print(f"Deleted {commands[1]} save file", end='\r')
-                    case 'l':
-                        print(f"Loaded {commands[1]} save file", end='\r')
-                    case 'n':
-                        print(f"New save file was created with name: {commands[1]}", end='\r')
-                    case _:
-                        print("Invalid option", end='')
-            case 'h':
-                clr_ln()
-                print('Help mode', end='\r')
-            case 'p':
-                clr_ln()
-                print('Play mode', end='\r')
-            case 's':
-                clr_ln()
-                print('Settings mode', end='\r')
-            case 'c':
-                clr_ln()
-                print('Credits', end='\r')
-            case '\033':
-                clr_ln()
-                break
-            case _:
-                clr_ln()
-                print('Unknown', end='\r')
+        if char == ':':
+            command = input(':')
+            clr_ln()
+            commands = command.split(' ')
+            match commands[0]:
+                case 'esc':
+                    continue
+                case 'wq':
+                    with open('./test.save', 'wb', encoding='utf-8') as f:
+                        f.write(pickle.dumps("smth"))
+                    break
+                case "w":
+                    with open('./test.save', 'wb', encoding='utf-8') as f:
+                        f.write(pickle.dumps("smth"))
+                case 'q!':
+                    print("force quit", end='\r')
+                    break
+                case 'x':
+                    print(f"Deleted {commands[1]} save file", end='\r')
+                case 'l':
+                    with open('./test.save', 'rb', encoding='utf-8') as f:
+                        pickle.loads(f.read)
+                    print(f"Loaded {commands[1]} save file", end='\r')
+                case 'n':
+                    with open(f'./{commands[1]}.save', 'wb', encoding='utf-8') as f:
+                        pass
+                case _:
+                    print("Invalid option", end='')
+        elif char == 'h' or '?':
+            clr_ln()
+            print('Help mode', end='\r')
+        elif char == 'p':
+            clr_ln()
+            print('Play mode', end='\r')
+        elif char == 's':
+            clr_ln()
+            print('Settings mode', end='\r')
+        elif char == 'c':
+            clr_ln()
+            print('Credits', end='\r')
+        else:
+            clr_ln()
+            print('Unknown', end='\r')
 
 if __name__ == '__main__':
     main()
